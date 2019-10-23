f802df2e635ca389ebb5b9d64180a2e0d6e27e01
Luis Miguel Ríos Pérez <54143281+luismrios1602@users.noreply.github.com>
2019-10-22 22:50:28 -0500

    Terminado Vectores con Funciones

--- PracticaVectores/VectoresConFunciones/Program.cs
+++ PracticaVectores/VectoresConFunciones/Program.cs
@@ -19,31 +19,47 @@ namespace VectoresConFunciones
 			Console.Write("Ingrese la cantidad de empleados: ");
 			cant = int.Parse(Console.ReadLine());
 			String [] nombres=new String[cant];
+			float [] salarios=new float[cant];
 			
 			Console.Clear();
-			for (int i = 0; i < cant; i++) {
-				nombres[i]=CargarVector(cant, i);
-			}
+			CargarVector(cant,nombres, salarios);
+			Console.Clear();
+			Console.WriteLine("Datos del empleado que más gana: ");
+			Console.WriteLine("Nombre: "+nombres[salMayor(salarios)]);
+			Console.WriteLine("Salario: "+salarios[salMayor(salarios)]);
 			
-			for (int i = 0; i < nombres.Length; i++) {
-				Console.WriteLine(nombres[i]);
-			}
+			
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
 		}
 		
-		public static String CargarVector(int cant, int i){
-				String [] nombres=new String[cant];
-				float [] salarios=new float[cant];
+		public static void CargarVector(int cant, String[] nombres, float[] salarios){
 				
-				Console.WriteLine("Ingrese la información del "+(i+1)+"° empleado: ");
-				Console.Write("Nombre: ");
-				nombres[i] = Console.ReadLine();
-				Console.Write("Salario: ");
-				salarios[i] = float.Parse(Console.ReadLine());
-				return nombres[i];
+				
+				for (int i = 0; i < nombres.Length; i++) {
+					
+					Console.WriteLine("Ingrese la información del "+(i+1)+"° empleado: ");
+					Console.Write("Nombre: ");
+					nombres[i] = Console.ReadLine();
+					Console.Write("Salario: ");
+					salarios[i] = float.Parse(Console.ReadLine());
+				
+				}
 			}
+		
+		public static int salMayor(float[] salarios){
+			float mayor=0;
+			int ubicacion=0;
+			for (int i = 0; i < salarios.Length; i++) {
+				if (salarios[i]>mayor) {
+					mayor=salarios[i];
+					ubicacion=i;
+				}
+			}
+			
+			return ubicacion;
+		}
 		
 	}
 }

c55db8a83b62421a5812da75949615a6e6b3e144
Luis Miguel Ríos Pérez <54143281+luismrios1602@users.noreply.github.com>
2019-10-22 22:12:59 -0500

    Ejercicio de empleados con Vectores

---
+++ PracticaVectores/Ejercicio1/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Lenovo
+ * Fecha: 22/10/2019
+ * Hora: 8:37 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace Ejercicio1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			String [] nombres = new string[5];
+			int [] salarios = new int[5];
+			int mayor=0; //Que me guarde el salario mayor
+			int ubicacion=0; //Que me almacene la ubicación del mayor
+			
+			//Pedir Datos
+			for (int i = 0; i < nombres.Length; i++) {
+				Console.WriteLine("Ingrese los datos del "+(i+1)+"° trabajador: ");
+				Console.Write("Nombre: ");
+				nombres[i] = Console.ReadLine();
+				Console.Write("Salario: $");
+				salarios[i] = int.Parse(Console.ReadLine());
+			}
+			
+			//Mostrar datos
+			for (int i = 0; i < salarios.Length; i++) {
+				if (salarios[i]>mayor) {
+					
+					mayor=salarios[i];
+					ubicacion=i;
+				}
+			}
+			
+			Console.WriteLine("El empleado con mayor salario es: ");
+			Console.WriteLine("Nombre: "+nombres[ubicacion]);
+			Console.WriteLine("Salario: $"+salarios[ubicacion]);
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ PracticaVectores/VectoresConFunciones/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Lenovo
+ * Fecha: 22/10/2019
+ * Hora: 9:06 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace VectoresConFunciones
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			int cant;
+			Console.Write("Ingrese la cantidad de empleados: ");
+			cant = int.Parse(Console.ReadLine());
+			String [] nombres=new String[cant];
+			
+			Console.Clear();
+			for (int i = 0; i < cant; i++) {
+				nombres[i]=CargarVector(cant, i);
+			}
+			
+			for (int i = 0; i < nombres.Length; i++) {
+				Console.WriteLine(nombres[i]);
+			}
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+		
+		public static String CargarVector(int cant, int i){
+				String [] nombres=new String[cant];
+				float [] salarios=new float[cant];
+				
+				Console.WriteLine("Ingrese la información del "+(i+1)+"° empleado: ");
+				Console.Write("Nombre: ");
+				nombres[i] = Console.ReadLine();
+				Console.Write("Salario: ");
+				salarios[i] = float.Parse(Console.ReadLine());
+				return nombres[i];
+			}
+		
+	}
+}

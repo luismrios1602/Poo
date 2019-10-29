9b19fb15c88ae2dfb2f736492e33a5da1bf935ec
Luis Miguel Ríos Pérez <54143281+luismrios1602@users.noreply.github.com>
2019-10-29 07:46:46 -0500

    ArrayList

---
+++ Colecciones/DemoArrayList/Carro.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: CBN
+ * Fecha: 29/10/2019
+ * Hora: 6:41
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace DemoArrayList
+{
+	/// <summary>
+	/// Description of Carro.
+	/// </summary>
+	public class Carro
+	{
+		public string placa, marca;
+		
+		public Carro()
+		{
+		}
+	}
+}
---
+++ Colecciones/DemoArrayList/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: CBN
+ * Fecha: 29/10/2019
+ * Hora: 6:28
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Collections;
+
+namespace DemoArrayList
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			ArrayList AL = new ArrayList();
+			
+			AL.Add("Mi primer ArrayList");
+			AL.Add(5);
+			Carro car=new Carro();
+			car.placa = "HFF-626";
+			car.marca = "Mazda";
+			AL.Add(car);
+			Carro carrito=(Carro)AL[2];
+			
+			Console.WriteLine("En la posición 0: "+AL[0]);
+			Console.WriteLine("En la posición 1: "+AL[1]);
+			Console.WriteLine("En la posición 2: "+carrito.marca);
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
